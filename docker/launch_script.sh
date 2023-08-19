ba677af6a9a6bef7f95298c72c2c9ac4bae42b5b
Joshua <joshua.cifuentes.romero@hotmail.com>
2023-08-19 18:52:42 +0200

    Ready API Front Container and USD_VALUE container with Mongo Ingestion

--- docker/launch_script.sh
+++ docker/launch_script.sh
@@ -3,3 +3,7 @@
 docker compose -f mongo_compose.yml up
 
 #There is a mongo express ready to see the databases http://localhost:8081/
+
+#FRONT API BUILDING
+docker-compose -f .\front_api_compose.yml build
+docker-compose -f .\front_api_compose.yml up
---
+++ python/usd_ingest_service/usd_ingest_service.py
@@ -0,0 +1,37 @@
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+from pymongo import MongoClient
+
+url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+parameters = {
+  'convert':'USD'
+}
+headers = {
+  'Accepts': 'application/json',
+  'X-CMC_PRO_API_KEY': '96772d27-f76f-4c82-b06d-03c8edaa6e26',
+}
+
+session = Session()
+session.headers.update(headers)
+
+try:
+  response = session.get(url)
+  data = json.loads(response.text)
+  client = MongoClient('mongodb://root:example@host.docker.internal', 27017)
+  print(len(data))
+  print(data.get("status"))
+  print(data.get("data"))
+
+  db = client['local']
+  collection_status = db['status']
+  collection_usd = db['USD_values']
+  # if pymongo >= 3.0 use insert_many() for inserting many documents
+  collection_status.insert_one(data.get("status"))
+  collection_usd.insert_many(data.get("data"))
+
+
+  client.close()
+except (ConnectionError, Timeout, TooManyRedirects) as e:
+  print(e)
+
